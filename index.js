e76424044c786c863b28c743f509b8d0c2add9aa
Joseph <joseph.larralde@gmail.com>
2020-12-16 04:42:49 +0100

    cleaning

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 
 import App from './src/App';
-// import App from './src/BlePlxApp';
-// import App from './src/BleManagerApp';
-// import App from './src/NewApp';
 
 import { Provider } from 'react-redux';
 import store from './src/store';
